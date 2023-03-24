f2c214df9f032aa2b4f1527bc531c50893b6cd0a
j4jeetu18 <j4jeetu18@gmail.com>
2023-03-24 16:25:14 +0530

    performed modifications for code redundancy

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -10,16 +10,16 @@ export default function Footer() {
             <div id="subscription">
                 <h3>Subscribe</h3>
                 <span>
-                <input placeholder="xyz@gmail.com" />
-                <button>Submit</button>
+                    <input placeholder="xyz@gmail.com" />
+                    <button>Submit</button>
                 </span>
             </div>
         </div>
         <div id="footer-div2">
             <span id="social-media-icons"><h3>Follow Us</h3>
-            <span style={{display:"flex",justifyContent:"space-around"}}>
-                <span style={{ fontSize: "35px", Width: "42px" }}><AiFillFacebook /></span>
-                <span style={{ fontSize: "35px", Width: "42px" }}><AiFillTwitterCircle /></span>
+                <span style={{ display: "flex", justifyContent: "space-around" }}>
+                    <span style={{ fontSize: "35px", Width: "42px" }}><AiFillFacebook /></span>
+                    <span style={{ fontSize: "35px", Width: "42px" }}><AiFillTwitterCircle /></span>
                 </span>
             </span>
             <span id="contact">
@@ -29,5 +29,5 @@ export default function Footer() {
                 <dt>Mail us:</dt><dl>dummy@gmail.com</dl>
             </span>
         </div>
-    </>)
-}
+    </>
+    )}
