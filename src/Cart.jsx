b7b5e0b4051663adeccf114a45c08350949b3e0e
j4jeetu18 <j4jeetu18@gmail.com>
2023-03-26 02:17:18 +0530

    made UI changes for responsivness

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -10,7 +10,7 @@ export default function Cart({ isCartVisible, setIsCartVisible }) {
     let cart = JSON.parse(localStorage.getItem("cart"))
 
     return (
-        <Drawer open={isCartVisible} onClose={() => setIsCartVisible(false)} placement="right" size="sm" backdrop={true} >
+        <Drawer open={isCartVisible} onClose={() => setIsCartVisible(false)} id="cart-drawer" placement="right" size="sm" backdrop={true} >
             <Drawer.Header style={{ backgroundColor: "#354131" }}>
                 <Drawer.Title><h3 style={{ fontFamily: "Montserrat Alternates', sans-serif", color: "#d9dfdf" }}>Cart</h3></Drawer.Title>
             </Drawer.Header>
