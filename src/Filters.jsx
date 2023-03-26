cbbec405c93f38e3a7af1b7c771873541dbae078
j4jeetu18 <j4jeetu18@gmail.com>
2023-03-26 08:19:43 +0530

    changes made to filter

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -12,7 +12,7 @@ export default function FIlters({ setFilterSelected }) {
             setFilterSelected([])
     }
     return (
-            <div style={{ width: "100%", marginBottom: "23px",display:"flex" }}>
+            <div id="filter-container">
                 <span style={{width:"20%"}}><h3>Filters</h3></span>
                 <div id="filters">
                 <span>
@@ -20,7 +20,7 @@ export default function FIlters({ setFilterSelected }) {
                 <input type="checkbox"/>
                 </span>
                 <span>
-                    <p>Out of stock</p>
+                    <p>Exclude Out of stock</p>
                 <input type="checkbox"/>
                 </span>
                 </div>
