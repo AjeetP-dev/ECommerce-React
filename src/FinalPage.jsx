86e4451867f0685249f787f474e1e32a492c5661
j4jeetu18 <113270076+j4jeetu18@users.noreply.github.com>
2023-03-29 13:21:04 +0530

    Final UI version (#19)
    * performed modifications for code redundancy
    
    * added responsivness
    
    * added responsivness
    
    * made UI changes for responsivness
    
    * changes made to filter
    
    * changes made to filter
    
    * trial version
    
    * Final UI version build 2903231251

---
+++ src/FinalPage.jsx
@@ -0,0 +1,11 @@
+export default function FinalPage(){
+    return(
+        <>
+        <div style={{display:"block",textAlign:"center",alignSelf:"start"}}>
+            <h1 style={{margin:"auto"}}>
+                Thanks for trying out the app
+            </h1>
+        </div>
+        </>
+    )
+}
